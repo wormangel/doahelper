829c34d5b35e216668f9c2ff82bfb19fc6436a1d
Lucas Medeiros de Azevedo <lucas.medeiros.de.azevedo@zalando.de>
2017-06-07 01:05:21 +0200

    bootstrap set up, scaffolding regenerated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606223957) do
+ActiveRecord::Schema.define(version: 20170606230112) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
