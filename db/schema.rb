0cf0b4e2ce3e3bfa0a303ea110664b90b90c63b4
Lucas Medeiros de Azevedo <lucas.medeiros.de.azevedo@zalando.de>
2017-06-07 03:18:23 +0200

    Add picture to character

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606231914) do
+ActiveRecord::Schema.define(version: 20170607011256) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   create_table "costumes", force: :cascade do |t|
