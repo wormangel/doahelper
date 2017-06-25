5ba468f1e3bdd7198d0a45badb45169ecc0f7cea
Lucas Azevedo <wormangel@gmail.com>
2017-06-25 14:37:27 +0200

    Missing image fix + gitgnore public assets + image urls helper file

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,8 +1,8 @@
 class Costume < ActiveRecord::Base
   belongs_to :character
 
-  has_attached_file :pic_official, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :pic_ingame, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :pic_official, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/:style/missing.png"
+  has_attached_file :pic_ingame, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/:style/missing.png"
   validates_attachment_content_type :pic_official, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_content_type :pic_ingame, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
